6d2eec598968d109d3e552affb64e225e188870c
hex <hex@hex.com>
2018-11-29 15:15:36 +0800

    test passed with slow speed

--- src/qz_bridge/src/cmd_vel_bridge.cpp
+++ src/qz_bridge/src/cmd_vel_bridge.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include "nav_msgs/Odometry.h"
 #include "std_srvs/SetBool.h"
 #include "DataClient.h"
 #include "DataHost.h"
@@ -7,6 +8,7 @@
 #include <unistd.h>
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
 
 static const char fmt[] = "tt --online --load=%d --start=%d --stop=%d --trot_vel=%.3f --turn_rate=%.3f";
 static const int LOAD = 9;
@@ -84,10 +86,63 @@ int main(int argc, char **argv)
 
     ros::Subscriber sub = n.subscribe("cmd_vel", 10, cmdVelCallback);
     ros::ServiceServer srv = n.advertiseService("cmd_start", cmdStartCallback);
+    ros::Publisher pub = n.advertise<nav_msgs::Odometry>("robot_odom", 10);
+    ros::Rate loop_rate(50);
+    int count = 0;
 
     client.Initialize(param, "RosCmdVelBridge");
 
-    ros::spin();
+    //ros::spin();
+
+    // pull robot state data from dataAgent and publish them to related ros nodes
+    while(ros::ok())
+    {
+        double turn_rate = 0;
+        double trot_vel = 0;
+        double side_vel = 0;
+
+        ros::spinOnce();
+
+        client.GetData(robot_libs::networking::RobotDataType::ROBOT_STATE, data_host);
+        client.GetData(robot_libs::networking::RobotDataType::CUSTOM_GAIT_DATA, data_host);
+
+        nav_msgs::Odometry msg;
+        auto* p_data = data_host.GetCustomGaitData()->data();
+
+        if (p_data && p_data->IsObject())
+        {
+            rapidjson::StringBuffer buf;
+            rapidjson::Writer<rapidjson::StringBuffer> writer(buf);
+            (*p_data).Accept(writer);
+            std::cout << buf.GetString() << std::endl;
+
+            if (p_data->HasMember("body_vel"))
+            {
+                rapidjson::Value &body_vel = (*p_data)["body_vel"];
+                if (body_vel.IsArray())
+                {
+                    trot_vel = body_vel[0].GetDouble();
+                    side_vel = -body_vel[1].GetDouble();
+                    turn_rate = body_vel[2].GetDouble();
+                }
+            }
+        }
+
+        ROS_INFO("Vel: %.4f, %.4f Rate: %.4f", trot_vel, side_vel, turn_rate);
+
+        msg.child_frame_id = "base_link";
+        msg.header.frame_id = "map";
+        msg.header.stamp = ros::Time::now();
+        
+        msg.twist.twist.linear.x = trot_vel;
+        msg.twist.twist.linear.y = side_vel;
+        msg.twist.twist.angular.z = turn_rate;
+
+        pub.publish(msg);
+
+        loop_rate.sleep();
+        ++count;
+    }
 
     return 0;
 }
